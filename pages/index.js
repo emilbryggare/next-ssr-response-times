7287cb9bb23f0aae16b26c41fadd3d20bd2a0290
Emil Bryggare <emil@emilbryggare.com>
2020-11-13 06:37:28 +0100

    add ssr

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
-import Nav from '../components/nav'
+import Nav from "../components/nav";
 
 export default function IndexPage() {
   return (
     <div>
       <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
-        </h1>
+      <div className='py-20'>
+        <h1 className='text-5xl text-center text-accent-1'>Next.js + Tailwind CSS</h1>
       </div>
     </div>
-  )
+  );
+}
+export async function getServerSideProps(context) {
+  return {
+    props: {},
+  };
 }
